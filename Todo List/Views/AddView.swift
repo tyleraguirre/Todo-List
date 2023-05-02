8f9490acc4d157975569848a579b9a77c9be28aa
tyleraguirre <taguirre6807@xmail.dixietech.edu>
2023-05-02 15:28:55 -0600

    added viewmodel and crud funcs

--- Todo List/Views/AddView.swift
+++ Todo List/Views/AddView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -20,21 +24,37 @@ struct AddView: View {
                     .background(.quaternary)
                 .cornerRadius(10)
                 
-                Button {
-                    
-                } label: {
+                Button(action: saveButtonPressed , label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
                         .font(.headline)
                         .frame(height: 55)
                         .frame(maxWidth: .infinity)
                         .background(Color.accentColor)
-                }
-
+                })
             }
             .padding(14)
         }
         .navigationTitle("Add an item 🖊️")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be 3 characters long 😳"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
@@ -43,5 +63,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView {
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
