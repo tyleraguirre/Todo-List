085b56a2878035860433908e32a97538a055f466
tyleraguirre <taguirre6807@xmail.dixietech.edu>
2023-05-08 13:21:29 -0600

    added userdefault to save items

--- Todo List/ViewModels/ListViewModel.swift
+++ Todo List/ViewModels/ListViewModel.swift
@@ -17,18 +17,23 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title", isCompleted: false),
-            ItemModel(title: "This is the second", isCompleted: true),
-            ItemModel(title: "Third", isCompleted: false)]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -49,6 +54,12 @@ class ListViewModel: ObservableObject {
 //       }
         if let index = items.firstIndex(where: {$0.id == item.id}) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
